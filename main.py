8cee8e7ab29b82b0a9014da0551181dfe5037fe7
kariebik <kekatimi@gmail.com>
2023-12-15 11:50:47 -0500

    initialise

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print("Hey, this is Lev who is in love with mendy")
+    print("kyle is here")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
