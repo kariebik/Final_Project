5854ab873c6b99cae1791446693a1284006796fc
MendyNguyen <nguyenboizz511@gmail.com>
2024-01-03 16:01:40 -0500

    mendy_work.py

---
+++ mendy_work.py
@@ -0,0 +1,12 @@
+import math
+
+angle_in_degrees = 55
+angle_in_radians = math.radians(angle_in_degrees)
+
+sin_value = math.sin(angle_in_radians)
+cos_value = math.cos(angle_in_radians)
+tan_value = math.tan(angle_in_radians)
+
+print("The sin of", angle_in_degrees, "degrees is:", sin_value)
+print("The cos of", angle_in_degrees, "degrees is:", cos_value)
+print("The tan of", angle_in_degrees, "degrees is:", tan_value)
